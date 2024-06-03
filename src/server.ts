776307bcf13e7966008d9ead21d5b6148ce8e7be
Felipe Diniz <felipedinizdossantos@gmail.com>
2024-06-03 16:43:45 -0400

    Update connection string

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,6 @@ import { routes } from './routes';
 
 import { errorMiddleware } from './middlewares/errorMiddleware';
 import helmet from 'helmet';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const createApp = () => {
     const app = express();
@@ -23,7 +20,7 @@ const createApp = () => {
 
 const startServer = () => {
     const app = createApp();
-    const PORT = process.env.PORT || 8000;
+    const PORT = 8000;
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
